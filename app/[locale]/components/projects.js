0d7339af0c508b25079246d27506e94e593bfac5
Esteban Horn <87593390+EstebanHorn@users.noreply.github.com>
2024-07-27 13:03:49 -0300

    Update projects.js

--- app/[locale]/components/projects.js
+++ app/[locale]/components/projects.js
@@ -32,7 +32,7 @@ export default function Porojects() {
           <h2 className="text-lg font-bold group-hover:text-blue-700 transition-all duration-200">Summoners Handbook</h2>
           <Image src={summoners} className="h-full object-cover" alt="Summoners Handbook screenshot"/>
         </Link>
-        <Link href="https://www.techosyservicios.com.ar/" className="flex flex-col items-center gap-4 pt-2 group bg-slate-100 hover:bg-slate-50 hover:scale-105 hover:-translate-y-1  transition-all duration-200">
+        <Link href="https://mah-construcciones.vercel.app/" className="flex flex-col items-center gap-4 pt-2 group bg-slate-100 hover:bg-slate-50 hover:scale-105 hover:-translate-y-1  transition-all duration-200">
         <h2 className="text-lg font-bold group-hover:text-blue-700 transition-all duration-200">MAH Construcciones</h2>
           <Image src={mah} className="h-full object-cover" alt="PalabraDeHoy app screenshot"/>
         </Link>
